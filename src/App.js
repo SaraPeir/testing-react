02f2b33bd2954511d84ac2d94aac4bb666d7e813
Sara Peirce <you@example.com>
2018-12-04 17:21:54 +0100

    added basic tests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import './app.css';
 
 class App extends Component {
   constructor() {
@@ -9,10 +10,15 @@ class App extends Component {
     };
   }
 
-  render() {    
+  render() {
+      const num1 = 1;  
+      const num2 = 2;  
+      const calculatedValue = num1 + num2; 
     return (
       <div className="App">
-        <h1>Hola!</h1>
+        <h1 id="greeting" className="red-text">Hola!</h1>
+        <h3 id="eng-greeting">Hello!</h3>
+        <p id="calculated-value">{calculatedValue}</p>
       </div>
     )
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-    shallow(<App />);
-});
+describe('<App />', () => {
+
+        it('renders without crashing', () => {
+            const wrapper = shallow(<App />);
+            expect(wrapper).toHaveLength(1);
+        });
+        
+        it('has a only red text', () => {
+            const wrapper = shallow(<App />);
+            expect(wrapper.find('.red-text')).toHaveLength(1);
+        });
+
+        it('has a only greeting id', () => {
+            const wrapper = shallow(<App />);
+            expect(wrapper.find('#greeting')).toHaveLength(1);
+        });
+
+        it('p with id="eng-greeting" contains "Hello!"', () => {
+            const calculatedValue = 5
+            const wrapper = shallow(
+                <p id="eng-greeting">Hello!</p>
+            );
+            expect(wrapper.contains("Hello!")).toEqual(true);
+        });
+
+        it('shows the correct calculated value', () => {
+            const calculatedValue = 5
+            const wrapper = shallow(
+                <p id="calculated-value">{calculatedValue}</p>
+            );
+            expect(wrapper.contains(5)).toEqual(true);
+        });
+
+        it('shows the correct calculated value 3', () => {
+            const calculatedValue = 5
+            const wrapper = shallow(
+                <p id="calculated-value">{calculatedValue}</p>
+            );
+            expect(wrapper.text()).toEqual(calculatedValue.toString());
+        });
+    }
+)
